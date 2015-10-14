92661c35149b62bed76348a5b1d4254e04f359e1
张婷华 <471148761@qq.com>
2015-10-14 16:36:02 +0800

    first time

---
+++ home.php
@@ -0,0 +1,34 @@
+<html>
+    <?php include 'inc_common_head.html'; ?>
+    <body id="home-page">
+        <?php include 'inc_header.html'; ?>
+        <div class="inner">
+            <div class="bkg-dark">
+                <div class="info-box">
+                    <div class="user-send">
+                        
+                        <div class="face-box"><a href=""><img src="files/images/face1.jpg"></a></div>
+                        <div class="send-list bubble-box">
+                            <i class="dec"></i>
+                            <a class="text"href="">Text</a>
+                            <a class="photo"href="">Photo</a>
+                            <a class="diary"href="">Diary</a>
+                            <a class="enjoyment"href="">Enjoyment</a>
+                        </div>
+                    </div>
+                    <div class="trends-box">
+                        <div class="face-box"><a href=""><img src="files/images/face1.jpg"></a></div>
+                        <div class="diary-box dynamic-box bubble-box">
+                            <i class="dec"></i>
+                            <p><a href="">timu</a><span><a href="">@user</a></span></p>
+                            <p>hehedadadadafesfldskfos fdsjflsdkjfosdf fsdjfldsjf fdsjlfkjdsf</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+
+    </body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+    <?php include 'inc_common_head.html'; ?>
+    <body id="login-page">
+        <div class="search-box">
+            <div class="input-box">
+                <span class="icon-glass"></span>
+                <input type="text" name="search" placeholder="查找"/>
+            </div>
+            <div class="search-result"></div>
+        </div>
+        <div class="middle-box">
+            <div class="logo">
+                <img src="files/images/logo.png">
+            </div>
+            <div class="login-box">
+                <div class="input-box">
+                    <input type="text" placeholder="请输入用户名"/><span class="icon-user">1</span>
+                </div>
+                <div class="input-box">
+                    <input type="text"placeholder="请输入密码"/><span class="icon-key">1</span>
+                </div>
+                <button class="btn btn-info">登&nbsp;&nbsp;录
+                </button>
+                <div class="forget">
+                    <a class="sign"href="">注册</a>
+                    <a href="">忘记密码？</a>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
+
